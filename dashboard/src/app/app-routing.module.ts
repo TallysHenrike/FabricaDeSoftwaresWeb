91837d6c3eed0a0230e83941e158b45b7afa536a
TallysHenrike <tallys.henrike@gmail.com>
2019-08-08 15:36:36 -0300

    Adicionado os componentes do menu
    Relatórios;
    Faturas;
    Produtos;
    Perfil.

--- dashboard/src/app/app-routing.module.ts
+++ dashboard/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { MainComponent } from './main/main.component';
+import { RelatoriosComponent } from './main/relatorios/relatorios.component';
+import { FaturasComponent } from './main/faturas/faturas.component';
+import { ProdutosComponent } from './main/produtos/produtos.component';
+import { PerfilComponent } from './main/perfil/perfil.component';
+
+const routes: Routes = [
+	{
+		path: '',
+		component: MainComponent,
+		children: [
+			{path: 'relatorios', component: RelatoriosComponent},
+			{path: 'faturas', component: FaturasComponent},
+			{path: 'produtos', component: ProdutosComponent},
+			{path: 'perfil', component: PerfilComponent}
+		]
+	}
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -6,13 +6,25 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavComponent } from './nav/nav.component';
 import { MainComponent } from './main/main.component';
+import { RelatoriosComponent } from './main/relatorios/relatorios.component';
+import { FaturasComponent } from './main/faturas/faturas.component';
+import { ProdutosComponent } from './main/produtos/produtos.component';
+import { PerfilComponent } from './main/perfil/perfil.component';
+import { CadastroComponent } from './main/produtos/cadastro/cadastro.component';
+import { ListagemComponent } from './main/produtos/listagem/listagem.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		HeaderComponent,
 		NavComponent,
-		MainComponent
+		MainComponent,
+		RelatoriosComponent,
+		FaturasComponent,
+		ProdutosComponent,
+		PerfilComponent,
+		CadastroComponent,
+		ListagemComponent
 	],
 	imports: [
 		BrowserModule,

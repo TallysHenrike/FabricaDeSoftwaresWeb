6714dac68c057a53f90e29f1f1de2d2996c8e3b7
TallysHenrike <tallys.henrike@gmail.com>
2019-08-09 17:55:38 -0300

    Cabeçalho

--- eventos/src/app/cabecalho/cabecalho.component.ts
+++ eventos/src/app/cabecalho/cabecalho.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, HostListener } from '@angular/core';
 
 @Component({
 	selector: 'cabecalho',
@@ -7,8 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CabecalhoComponent implements OnInit {
 
-	constructor() { }
+	constructor(private element: ElementRef) { }
 
 	ngOnInit() { }
+
+	@HostListener('window:scroll', ['$event'])
+	onScroll(): void{
+		let parallax = this.element.nativeElement;
+
+		if(window.scrollY < parallax.offsetHeight){
+			let suave = window.scrollY * 70 / 100;
+			//console.log(parallax.offsetHeight, window.scrollY);
+			parallax.style.backgroundPosition = `center top ${suave}px`;
+		}
+
+		let menu = this.element.nativeElement.querySelector('#menu');
+		
+		console.log(window.scrollY >= (parallax.scrollHeight - menu.scrollHeight))
+		if(window.scrollY >= (parallax.scrollHeight - menu.scrollHeight)){
+			menu.style.position = 'fixed';
+			menu.style.backgroundColor = 'rgba(66, 66, 66, 1)';
+		}else{
+			menu.style.position = 'inherit';
+			menu.style.backgroundColor = 'rgba(66, 66, 66, .2)';
+		}
+	}
 
 }

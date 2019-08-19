89f265bf6469a0721caecef454e5a86eee36c274
Aluno <aluno@aluno.com>
2019-08-19 19:55:25 -0300

    Criado form-usuario

---
+++ src/app/principal/evento/inscrever/form-usuario/form-usuario.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'form-usuario',
+	templateUrl: './form-usuario.component.html',
+	styleUrls: ['./form-usuario.component.css']
+})
+export class FormUsuarioComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit() { }
+
+}

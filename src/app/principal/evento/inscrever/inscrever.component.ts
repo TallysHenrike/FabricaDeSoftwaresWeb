54afa9ca1b984f79cc2e8007ad0b5ac240ebff71
TallysHenrike <tallys.henrike@gmail.com>
2019-08-19 14:27:53 -0300

    Criando formulário para inscrição

--- src/app/principal/evento/inscrever/inscrever.component.ts
+++ src/app/principal/evento/inscrever/inscrever.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
 	selector: 'inscrever',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./inscrever.component.css']
 })
 export class InscreverComponent implements OnInit {
+
+	perfil = new FormGroup({
+		firstName: new FormControl(''),
+		lastName: new FormControl(''),
+	});
 
 	constructor() { }
 
